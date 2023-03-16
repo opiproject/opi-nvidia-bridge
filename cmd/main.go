8912a30cd4a170521637300b40669ff3141c79a9
Boris Glimcher <Boris.Glimcher@emc.com>
2023-03-16 15:16:13 +0200

    main: allow SPDK to be TCP as well as UNIX
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

--- cmd/main.go
+++ cmd/main.go
@@ -31,8 +31,8 @@ import (
 func main() {
 	var port int
 	flag.IntVar(&port, "port", 50051, "The Server port")
-	var spdkSocket string
-	flag.StringVar(&spdkSocket, "rpc_sock", "/var/tmp/spdk.sock", "Path to SPDK JSON RPC socket")
+	var spdkAddress string
+	flag.StringVar(&spdkAddress, "spdk_addr", "/var/tmp/spdk.sock", "Points to SPDK unix socket/tcp socket to interact with")
 	flag.Parse()
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
@@ -41,7 +41,7 @@ func main() {
 	}
 
 	s := grpc.NewServer()
-	jsonRPC := server.NewSpdkJSONRPC(spdkSocket)
+	jsonRPC := server.NewSpdkJSONRPC(spdkAddress)
 	frontendOpiNvidiaServer := fe.NewServer(jsonRPC)
 	frontendOpiSpdkServer := frontend.NewServer(jsonRPC)
 	backendOpiSpdkServer := backend.NewServer(jsonRPC)

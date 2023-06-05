35e4187743b10b1f304083fd823b9d13f26630c8
Boris Glimcher <Boris.Glimcher@emc.com>
2023-06-05 19:55:03 +0300

    Following Google AIP-134 - Standard methods: Update
    Adding `allow_missing` bool field
    
    see https://google.aip.dev/134#create-or-update
    
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

--- pkg/frontend/virtio_test.go
+++ pkg/frontend/virtio_test.go
@@ -94,11 +94,26 @@ func TestFrontEnd_UpdateVirtioBlk(t *testing.T) {
 		start   bool
 	}{
 		"unimplemented method": {
-			&pb.VirtioBlk{},
+			&pb.VirtioBlk{
+				Name: testVirtioCtrlID,
+			},
 			nil,
 			[]string{""},
 			codes.Unimplemented,
 			fmt.Sprintf("%v method is not implemented", "UpdateVirtioBlk"),
+			false,
+		},
+		"valid request with unknown key": {
+			&pb.VirtioBlk{
+				Name:     "unknown-id",
+				PcieId:   &pb.PciEndpoint{PhysicalFunction: 42},
+				VolumeId: &pc.ObjectKey{Value: "Malloc42"},
+				MaxIoQps: 1,
+			},
+			nil,
+			[]string{""},
+			codes.NotFound,
+			fmt.Sprintf("unable to find key %v", "unknown-id"),
 			false,
 		},
 	}
@@ -108,6 +123,8 @@ func TestFrontEnd_UpdateVirtioBlk(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			testEnv := createTestEnvironment(tt.start, tt.spdk)
 			defer testEnv.Close()
+
+			testEnv.opiSpdkServer.VirtioCtrls[testVirtioCtrl.Name] = &testVirtioCtrl
 
 			request := &pb.UpdateVirtioBlkRequest{VirtioBlk: tt.in}
 			response, err := testEnv.client.UpdateVirtioBlk(testEnv.ctx, request)

3d074109fbefcdddb14fd954067f48a644bc5233
Artsiom Koltun <artsiom.koltun@intel.com>
2023-07-04 13:39:16 +0200

    Test controller update with non-compliant name.
    Signed-off-by: Artsiom Koltun <artsiom.koltun@intel.com>

--- pkg/frontend/nvme_controller_test.go
+++ pkg/frontend/nvme_controller_test.go
@@ -339,6 +339,15 @@ func TestFrontEnd_UpdateNvmeController(t *testing.T) {
 			fmt.Sprintf("unable to find key %v", server.ResourceIDToVolumeName("unknown-id")),
 			false,
 		},
+		"malformed name": {
+			nil,
+			&pb.NvmeController{Name: "-ABC-DEF"},
+			nil,
+			[]string{},
+			codes.Unknown,
+			fmt.Sprintf("segment '%s': not a valid DNS name", "-ABC-DEF"),
+			false,
+		},
 	}
 
 	// run tests
